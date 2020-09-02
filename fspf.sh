3d40203cdb6e3515ef0fe35549333d9ed0f213dc
Clenimar Souza <clenimar.filemon@gmail.com>
2020-09-02 14:20:41 +0000

    run demo with glibc python

--- fspf.sh
+++ fspf.sh
@@ -7,6 +7,12 @@ scone fspf addr /fspf/fspf-file/fs.fspf / --not-protected --kernel /
 # add encrypted region /fspf/encrypted-files
 scone fspf addr /fspf/fspf-file/fs.fspf /fspf/encrypted-files/ --encrypted --kernel /fspf/encrypted-files/
 # add all files in directory /fspf/native-files/ to /fspf/encrypted-files/
-scone fspf addf /fspf/fspf-file/fs.fspf /fspf/encrypted-files/ /fspf/native-files/ /fspf/encrypted-files/ 
+scone fspf addf /fspf/fspf-file/fs.fspf /fspf/encrypted-files/ /fspf/native-files/ /fspf/encrypted-files/
+# authenticate glibc and related libs
+scone fspf addr /fspf/fspf-file/fs.fspf /opt/scone/lib --authenticated --kernel /opt/scone/lib
+scone fspf addf /fspf/fspf-file/fs.fspf /opt/scone/lib /opt/scone/lib
+# authenticate python interpreter
+scone fspf addr /fspf/fspf-file/fs.fspf /root/miniconda --authenticated --kernel /root/miniconda
+scone fspf addf /fspf/fspf-file/fs.fspf /root/miniconda /root/miniconda
 # finally, encrypt the file system protection file and store the keys in directory (we assume in this demo that wee run on a trusted host)
 scone fspf encrypt /fspf/fspf-file/fs.fspf > /fspf/native-files/keytag
