015e675dad7147bc16e06bdc81dbc71e7dbf5229
Christof Fetzer <christof.fetzer@gmail.com>
2020-09-14 11:23:05 +0200

    changed to redis community version

--- create_image.sh
+++ create_image.sh
@@ -22,8 +22,8 @@ export CAS_MRENCLAVE="460e24c965a94fd3718cb22472926c9517fb2912d2c8ca97ea26228e14
 export CLI_IMAGE="sconecuratedimages/kubernetes:hello-k8s-scone0.1"
 export PYTHON_IMAGE="sconecuratedimages/apps:python-3.7.3-alpine3.10-scone4.2"
 export PYTHON_MRENCLAVE="67b8017f7083435cb614b87c8daa14303f741a10a2a0bbf5dfabec777cf629b9"
-export REDIS_IMAGE="sconecuratedimages/experimental:redis-6-ubuntu"
-export REDIS_MRENCLAVE="60c87d30d609afd79d9c0af2b211ac30291d72e8989c1c6895d9aa3703b28882"
+export REDIS_IMAGE="sconecuratedimages/apps:redis-6-alpine-scone4"
+export REDIS_MRENCLAVE="32c0dcbbfcfc951fc21c8f611227e50593e5e759c1d659c63575078c35fadb9b"
 
 # create random and hence, uniquee session number
 FLASK_SESSION="FlaskSession-$RANDOM-$RANDOM-$RANDOM"
